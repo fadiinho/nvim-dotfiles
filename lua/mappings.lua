12802ae5d28b49b3c90d3f7ac396f87b605542d1
fadiinho <ffadiinho@gmail.com>
2022-09-21 17:23:28 -0300

    feat: add telescope keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -83,4 +83,124 @@ maps.v["<leader>/"] = {
   { desc = "Toggle comment line" },
 }
 
+-- Telescope
+maps.n["<leader>fw"] = {
+  function()
+    require("telescope.builtin").live_grep()
+  end,
+  desc = "Search words",
+}
+maps.n["<leader>fW"] = {
+  function()
+    require("telescope.builtin").live_grep {
+      additional_args = function(args)
+        return vim.list_extend(args, { "--hidden", "--no-ignore" })
+      end,
+    }
+  end,
+  desc = "Search words in all files",
+}
+maps.n["<leader>gt"] = {
+  function()
+    require("telescope.builtin").git_status()
+  end,
+  desc = "Git status",
+}
+maps.n["<leader>gb"] = {
+  function()
+    require("telescope.builtin").git_branches()
+  end,
+  desc = "Git branches",
+}
+maps.n["<leader>gc"] = {
+  function()
+    require("telescope.builtin").git_commits()
+  end,
+  desc = "Git commits",
+}
+maps.n["<leader>ff"] = {
+  function()
+    require("telescope.builtin").find_files()
+  end,
+  desc = "Search files",
+}
+maps.n["<leader>fF"] = {
+  function()
+    require("telescope.builtin").find_files { hidden = true, no_ignore = true }
+  end,
+  desc = "Search all files",
+}
+maps.n["<leader>fb"] = {
+  function()
+    require("telescope.builtin").buffers()
+  end,
+  desc = "Search buffers",
+}
+maps.n["<leader>fm"] = {
+  function()
+    require("telescope.builtin").marks()
+  end,
+  desc = "Search marks",
+}
+maps.n["<leader>fo"] = {
+  function()
+    require("telescope.builtin").oldfiles()
+  end,
+  desc = "Search history",
+}
+maps.n["<leader>fc"] = {
+  function()
+    require("telescope.builtin").grep_string()
+  end,
+  desc = "Search for word under cursor",
+}
+maps.n["<leader>sb"] = {
+  function()
+    require("telescope.builtin").git_branches()
+  end,
+  desc = "Git branches",
+}
+maps.n["<leader>sh"] = {
+  function()
+    require("telescope.builtin").help_tags()
+  end,
+  desc = "Search help",
+}
+maps.n["<leader>sm"] = {
+  function()
+    require("telescope.builtin").man_pages()
+  end,
+  desc = "Search man",
+}
+maps.n["<leader>sr"] = {
+  function()
+    require("telescope.builtin").registers()
+  end,
+  desc = "Search registers",
+}
+maps.n["<leader>sk"] = {
+  function()
+    require("telescope.builtin").keymaps()
+  end,
+  desc = "Search keymaps",
+}
+maps.n["<leader>sc"] = {
+  function()
+    require("telescope.builtin").commands()
+  end,
+  desc = "Search commands",
+}
+maps.n["<leader>lR"] = {
+  function()
+    require("telescope.builtin").lsp_references()
+  end,
+  desc = "Search references",
+}
+maps.n["<leader>lD"] = {
+  function()
+    require("telescope.builtin").diagnostics()
+  end,
+  desc = "Search diagnostics",
+}
+
 utils.set_keymaps(maps)
