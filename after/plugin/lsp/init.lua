1318130e3953a8887b66eb4b6f7e036ab4a73df9
fadiinho <ffadiinho@gmail.com>
2022-09-22 19:08:49 -0300

    feat: add null-ls auto format and disable formatting on lsp

--- after/plugin/lsp/init.lua
+++ after/plugin/lsp/init.lua
@@ -1,33 +1,33 @@
 local status, _ = pcall(require, "lspconfig")
 if not status then
-	return
+  return
 end
 
 local signs = {
-	{ name = "DiagnosticSignError", text = "" },
-	{ name = "DiagnosticSignWarn", text = "" },
-	{ name = "DiagnosticSignHint", text = "" },
-	{ name = "DiagnosticSignInfo", text = "" },
+  { name = "DiagnosticSignError", text = "" },
+  { name = "DiagnosticSignWarn", text = "" },
+  { name = "DiagnosticSignHint", text = "" },
+  { name = "DiagnosticSignInfo", text = "" },
 }
 for _, sign in ipairs(signs) do
-	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
 end
 
-vim.diagnostic.config({
-	virtual_text = true,
-	signs = { active = signs },
-	update_in_insert = true,
-	underline = true,
-	severity_sort = true,
-	float = {
-		focused = false,
-		style = "minimal",
-		border = "rounded",
-		source = "always",
-		header = "",
-		prefix = "",
-	},
-})
+vim.diagnostic.config {
+  virtual_text = true,
+  signs = { active = signs },
+  update_in_insert = true,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focused = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
+}
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
@@ -39,54 +39,63 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 vim.keymap.set("n", "<leader>ll", vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set("n", "<leader>wl", function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, bufopts)
-	vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, bufopts)
-	vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set("n", "<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 end
 
 local formatting_callback = function(client, bufnr)
-	vim.keymap.set("n", "<leader>f", function()
-		local params = vim.lsp.util.make_formatting_params({})
-		client.request("textDocument/formatting", params, nil, bufnr)
-	end, { buffer = bufnr })
+  vim.keymap.set("n", "<leader>f", function()
+    local params = vim.lsp.util.make_formatting_params {}
+    client.request("textDocument/formatting", params, nil, bufnr)
+  end, { buffer = bufnr })
 end
 
 local function disable_formatting(client)
-	client.resolved_capabilities.document_formatting = false
-	client.resolved_capabilities.document_range_formatting = false
+  client.resolved_capabilities.document_formatting = false
+  client.resolved_capabilities.document_range_formatting = false
 end
 
-require("lspconfig").sumneko_lua.setup({
-	on_attach = function(client, bufnr)
-		formatting_callback(client, bufnr)
-		disable_formatting(client)
-		on_attach(client, bufnr)
-	end,
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VINRUNTIME/lua/vim/lsp")] = true,
-				},
-			},
-		},
-	},
-})
+local function disable_formatting_and_attach(client, bufnr)
+  disable_formatting(client)
+  on_attach(client, bufnr)
+end
+
+require("lspconfig").pylsp.setup {
+  on_attach = function(client, bufnr)
+    disable_formatting_and_attach(client, bufnr)
+  end,
+}
+
+require("lspconfig").sumneko_lua.setup {
+  on_attach = function(client, bufnr)
+    disable_formatting_and_attach(client, bufnr)
+  end,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.expand "$VINRUNTIME/lua/vim/lsp"] = true,
+        },
+      },
+    },
+  },
+}
--- after/plugin/null-ls.rc.lua
+++ after/plugin/null-ls.rc.lua
@@ -1,37 +1,39 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-	return
+  return
 end
 
+-- For ez access: https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.prettier.with({
-			prefer_local = true,
-			filetypes = {
-				"css",
-				"markdown",
-				"json",
-				"jsonc",
-				"html",
-			},
-		}),
-		null_ls.builtins.formatting.rome.with({
-			prefer_local = true,
-			filetypes = { "javascript", "typescript" },
-		}),
-	},
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.formatting_sync()
-				end,
-			})
-		end
-	end,
-})
+null_ls.setup {
+  sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.prettier.with {
+      prefer_local = true,
+      filetypes = {
+        "css",
+        "markdown",
+        "json",
+        "jsonc",
+        "html",
+      },
+    },
+    null_ls.builtins.formatting.rome.with {
+      prefer_local = true,
+      filetypes = { "javascript", "typescript" },
+    },
+    null_ls.builtins.formatting.black,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
+}
