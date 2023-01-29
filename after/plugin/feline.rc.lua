54831c054517860c4f14911f58bd0df7447cf55d
fadiinho <ffadiinho@gmail.com>
2023-01-29 00:07:27 -0300

    feat: add load progress for feline

--- after/plugin/feline.rc.lua
+++ after/plugin/feline.rc.lua
@@ -89,6 +89,22 @@ local function get_vi_mode()
   }
 end
 
+function lsp_progress()
+  if #vim.lsp.buf_get_clients() == 0 then
+    return ""
+  end
+
+  local lsp = vim.lsp.util.get_progress_messages()[1]
+  if lsp then
+    local name = lsp.name or ""
+    local msg = lsp.message or ""
+    local percentage = lsp.percentage or 0
+    local title = lsp.title or ""
+    return string.format(" %%<%s: %s %s (%s%%%%) ", name, title, msg, percentage)
+  end
+  return ""
+end
+
 local SPACERS = {
   left = "",
   mid = "▊",
@@ -208,6 +224,12 @@ local middle = {
     end,
   },
   { " " },
+  {
+    provider = function()
+      return lsp_progress()
+    end,
+    left_sep = " ",
+  },
 }
 
 local right = {
